cd007a7e1f5515726073b8d8002c3e469657f017
Rafael <wickedxwayxgaming@gmail.com>
2023-03-30 18:54:52 -0700

    yooooo

--- script.js
+++ script.js
@@ -1,24 +1,18 @@
-
-  
-//   TODO define a counter variable that is mutable
-
-  
+let counter =0;
 let counterElement = document.getElementById("counter");
 
 function updateCounter() {
-  counterElement.innerText = counter
+  counterElement.innerText = counter;
 }
 
 function handleIncrement() {
-  // TODO update count by adding 1
-  
-  updateCounter();
+  counter +=1;
+ updateCounter();
 }
 
 function handleDecrement() {
-  // TODO update count by subtracting 1
-  
-  updateCounter();
+counter -=1;
+updateCounter();
 }
 
 let incrementButton = document.getElementById("increment-button");
